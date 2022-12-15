c50dc1bb31d957c234c2ba8f788f4807d94ff1e9
Vera <mokeeva.vk@phystech.edu>
2022-12-15 15:44:23 +0300

    filter and search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,7 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-class App extends Component { 
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -17,9 +17,11 @@ class App extends Component {
                 {name:'Alhaitham', salary: 2500, increase: true, rise: false, id: 2},
                 {name:'Tartaglia', salary: 4200, increase: false, rise: false, id: 3},
                 {name:'Zhongli', salary: 80, increase: false, rise: false, id: 4}
-            ]
+            ],
+            term: '', 
+            filter: 'all'
         }
-        this.maxId = 4;
+        this.maxId = 5;
     }
 
     deleteItem = (id) => {
@@ -32,56 +34,83 @@ class App extends Component {
 
     addItem = (name, salary) => {
         const newItem = {
-            name,
+            name, 
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
-
         this.setState(({data}) => {
             const newArr = [...data, newItem];
             return {
                 data: newArr
             }
-        })
+        });
     }
 
     onToggleProp = (id, prop) => {
-        this.setState(({data}) => {
-            const index = data.findIndex(elem => elem.id === id);
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
 
-            const old = data[index];
-            const newItem = {...old, [prop]: !old[prop]};
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
 
-            return {
-                data: newArr
-            }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
         })
     }
 
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
-    
+                
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
-                <EmployeesAddForm
-                    onAdd={this.addItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
- }
+}
 
- export default App;
+export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -12,19 +12,19 @@ class EmployeesAddForm extends Component {
     }
 
     onValueChange = (e) => {
-        this.setState ({
-            [e.target.name]: e.target.value
+        this.setState({
+            [e.target.name] : e.target.value
         })
     }
 
     onSubmit = (e) => {
         e.preventDefault();
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
             salary: ''
         })
-
     }
 
     render() {
@@ -35,23 +35,22 @@ class EmployeesAddForm extends Component {
                 <h3>Добавьте нового сотрудника</h3>
                 <form
                     className="add-form d-flex"
-                    onSubmit={this.onSubmit}>
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
-                        placeholder="Как его зовут?" 
+                        placeholder="Как его зовут?"
                         name="name"
-                        value={name}
+                        value={name} 
                         onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
-                        placeholder="З/П в $?" 
+                        placeholder="З/П в $?"
                         name="salary"
-                        value={salary}
+                        value={salary} 
                         onChange={this.onValueChange}/>
     
                     <button type="submit"
-                            className="btn btn-outline-light"
-                            onClick={this.onSubmit}>Добавить</button>
+                            className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
